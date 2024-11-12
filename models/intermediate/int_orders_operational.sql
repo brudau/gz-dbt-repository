e2987a4ff3b41713677860f47f8dc9e011d15fe7
BRUNO <bdnok520@outlook.fr>
2024-11-12 15:56:17 +0000

    test models

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,16 +2,15 @@ select
 m.orders_id
 , date_date
 , round(sum((margin+ shipping_fee-log_cost-ship_cost)),2)  as operational_margin
-, revenue
-, quantity
-,purchase_cost
-, margin
-from {{ ref('int_sales_margin') }} m
+, sum(revenue) as revenue
+, sum(quantity) as quantity
+,sum(purchase_cost) as purchase_cost 
+, sum(margin) as margin
+from {{ ref('int_orders_margin') }} m
 left join {{ ref('stg_raw__ship') }} sh
 on m.orders_id = sh.orders_id
 group by m.orders_id
 , m.date_date
-, revenue
-, quantity
-,purchase_cost
-, margin
+
+
+
