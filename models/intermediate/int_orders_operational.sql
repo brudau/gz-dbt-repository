334bf965507eeee81304c70f26517cd8f5f29bbb
BRUNO <bdnok520@outlook.fr>
2024-11-12 14:31:23 +0000

    int_ope_margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,17 @@
+select 
+m.orders_id
+, date_date
+, round(sum((margin+ shipping_fee-log_cost-ship_cost)),2)  as operational_margin
+, revenue
+, quantity
+,purchase_cost
+, margin
+from {{ ref('int_orders_margin') }} m
+left join {{ ref('stg_raw__ship') }} sh
+on m.orders_id = sh.orders_id
+group by m.orders_id
+, m.date_date
+, revenue
+, quantity
+,purchase_cost
+, margin
